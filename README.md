a939aa09913358fc3d19ec06aecb847dcf084d77
Nick French <64120654+nickjfrench@users.noreply.github.com>
2025-03-26 15:54:35 -0400

    Continuing book.

--- README.md
+++ README.md
@@ -2,11 +2,13 @@
 Code is being developed while reading the Let's Go book by Alex Edwards.
 For personal interest, I changed the scope from a `Code Snippet Hosting` to a `URL Shortener` service.
 
-This is for personal use-case for my website: [nickfrench.dev](https://nickfrench.dev)
-
 ## Scope
 
+- Simple HTML UI - little to no CSS
 - Accept any URL and output a shortened URL.
 - URL redirection mapping stored in DB.
-- Anyone can access redirection.
-- Only admins can access create form and view page.
+
+## Project Structure
+
+Using the [Go Doc's Server Project](https://go.dev/doc/modules/layout#server-project) file structure.
+TLDR: `internal` represents packages used by the server. `cmd/web` represents the Go code to run the server.
--- main.go
+++ main.go
@@ -27,12 +27,18 @@ func shortenerView(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	msg := fmt.Sprintf("Display a specific shortened URL with ID %d...", id)
-	w.Write([]byte(msg))
+	fmt.Fprintf(w, "Display a specific shortened URL with ID %d...", id)
 }
 
 func shortenerCreate(w http.ResponseWriter, r *http.Request) {
+
 	w.Write([]byte("Display a form for creating a shortened URL..."))
+}
+
+func shortenerCreatePost(w http.ResponseWriter, r *http.Request) {
+
+	w.WriteHeader(http.StatusCreated)
+	w.Write([]byte("Save a new shortened URL..."))
 }
 
 func main() {
@@ -40,11 +46,12 @@ func main() {
 	mux := http.NewServeMux()
 
 	// Define routes.
-	mux.HandleFunc("/{$}", home)
-	mux.HandleFunc("/health", health)
-	mux.HandleFunc("/shortener", shortenerList)
-	mux.HandleFunc("/shortener/view/{id}", shortenerView) // Including '/view/...' to avoid overlaps
-	mux.HandleFunc("/shortener/create", shortenerCreate)
+	mux.HandleFunc("GET /{$}", home)
+	mux.HandleFunc("GET /health", health)
+	mux.HandleFunc("GET /shortener", shortenerList)
+	mux.HandleFunc("GET /shortener/view/{id}", shortenerView) // Including '/view/...' to avoid overlaps
+	mux.HandleFunc("GET /shortener/create", shortenerCreate)
+	mux.HandleFunc("POST /shortener/create", shortenerCreatePost)
 
 	// Note log instead of fmt.PrintLn
 	log.Print("starting server on :4000")
