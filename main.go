c181f99af1219df0885ab628b894836c29bcf00f
Nick French <64120654+nickjfrench@users.noreply.github.com>
2025-03-25 20:45:55 -0400

    Continuing book.

--- main.go
+++ main.go
@@ -1,20 +1,38 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"strconv"
 )
+
+func home(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello World"))
+}
 
 func health(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("OK"))
 }
 
+func shortenerList(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Display all shortened URLs..."))
+}
+
 func shortenerView(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Display one shortened URL."))
+	// Get ID and convert to int.
+	id, err := strconv.Atoi(r.PathValue("id"))
+	if err != nil || id < 1 {
+		http.NotFound(w, r)
+		return
+	}
+
+	msg := fmt.Sprintf("Display a specific shortened URL with ID %d...", id)
+	w.Write([]byte(msg))
 }
 
 func shortenerCreate(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Display a form for creating a shortened URL."))
+	w.Write([]byte("Display a form for creating a shortened URL..."))
 }
 
 func main() {
@@ -22,9 +40,11 @@ func main() {
 	mux := http.NewServeMux()
 
 	// Define routes.
+	mux.HandleFunc("/{$}", home)
 	mux.HandleFunc("/health", health)
-	mux.HandleFunc("/shortener/view", shortenerCreate)
-	mux.HandleFunc("/shortener/create", shortenerView)
+	mux.HandleFunc("/shortener", shortenerList)
+	mux.HandleFunc("/shortener/view/{id}", shortenerView) // Including '/view/...' to avoid overlaps
+	mux.HandleFunc("/shortener/create", shortenerCreate)
 
 	// Note log instead of fmt.PrintLn
 	log.Print("starting server on :4000")
