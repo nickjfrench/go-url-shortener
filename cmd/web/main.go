32dd263fe7672132c924c62dfbc1fca2ba482983
Nick French <64120654+nickjfrench@users.noreply.github.com>
2025-04-01 22:33:39 -0400

    Integrating pgx package and initiating setup

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"context"
 	"flag"
+	"github.com/jackc/pgx/v5"
+	"github.com/joho/godotenv"
 	"log/slog"
 	"net/http"
 	"os"
@@ -14,6 +17,7 @@ type application struct {
 }
 
 func main() {
+	// CLI flags
 	addr := flag.String("addr", ":4000", "HTTP network address")
 	flag.Parse()
 
@@ -26,6 +30,28 @@ func main() {
 	// Logger Handler
 	logger := slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{AddSource: true, Level: logLevel}))
 
+	// Load .env file
+	envErr := godotenv.Load()
+	if envErr != nil {
+		logger.Error("Error loading .env file", slog.Any("error", envErr))
+	}
+
+	// DB - using Direct Connection, not a Pool.
+	// Note: May need to be changed to Session Pooler and pgxpool
+	conn, dbErr := pgx.Connect(context.Background(), os.Getenv("DATABASE_URL"))
+	if dbErr != nil {
+		logger.Error("Failed to connect to the database", slog.Any("error", dbErr))
+	}
+	defer conn.Close(context.Background())
+
+	// Example query to test connection
+	var version string
+	if err := conn.QueryRow(context.Background(), "SELECT version()").Scan(&version); err != nil {
+		conn.Close(context.Background())
+		logger.Error("Query failed", slog.Any("error", err))
+	}
+	logger.Info("Successfully connected to DB", slog.String("version", version))
+
 	// Application with DI
 	app := &application{
 		logger: logger,
@@ -35,8 +61,7 @@ func main() {
 	logger.Info("starting server", slog.String("addr", *addr))
 
 	// Start http listener using ServeMux route handler.
-	err := http.ListenAndServe(*addr, app.routes())
-	if err != nil {
+	if err := http.ListenAndServe(*addr, app.routes()); err != nil {
 		logger.Error(err.Error())
 		os.Exit(1) // Exit application on error.
 	}
